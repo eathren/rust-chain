e3fa5bf4461fafbdea4726b18cf990fcb2e021aa
Nolan <nolan.braman@gmail.com>
2021-10-21 16:05:04 -0400

    example Block and Transaction instances created. Test addition to block, and print to cmndline

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod block;
 mod transaction;
 use crate::block::Block;
+use crate::transaction::Transaction;
 
 fn main() {
-    println!("Hello, world!");
-    let block = Block::new( 10, 10, String::from("Test"),  4, 8, 7, Vec::new());
+    let transaction = Transaction::new(10, String::from("me"), String::from("you"), 10000);
+    // Should blocks be mutable? Usually Crypto is not. 
+    let mut block = Block::new( 10, 10, String::from("Test"),  4, 8, 7, Vec::with_capacity(1000));
+    block.add_transaction(transaction);
     println!("{:?}", block)
 }
