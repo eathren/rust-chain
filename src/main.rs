f275f35e2323e353a777c112a453e91be787cd3c
Nolan <nolan.braman@gmail.com>
2021-10-21 15:27:30 -0400

    updated main.rs to print test block

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod block;
 mod transaction;
+use crate::block::Block;
 
 fn main() {
     println!("Hello, world!");
+    let block = Block::new( 10, 10, String::from("Test"),  4, 8, 7, Vec::new());
+    println!("{:?}", block)
 }
